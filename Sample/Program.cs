9ff9b7b1bdb31f7cc4f3f70a0f553f2b2f685bcd
Pranav Karkhanis <pranavgk@gmail.com>
2019-07-23 13:05:14 -0700

    Update Program.cs

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -16,11 +16,8 @@ namespace Sample
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
